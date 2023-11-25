dcb9105da7bc405ceb8e95a47aef1c938cceacfc
yejun <yejun.yun2006@gmail.com>
2023-11-24 22:36:03 -0600

    minor

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ nextflow run pgscatalog/pgsc_calc \
     -profile docker \
     --input samplesheet.csv \
     --target_build GRCh37 \
-    --pgs_id PGS001229 \
+    --pgs_id PGS000758 \
 
 
 #recodes ped to bed
@@ -13,3 +13,4 @@ plink --file <ped_input_files> --make-bed --out <bed_output>
 
 #scores using plink --score
 plink2 --bfile <bed_input_files> --score <score_file> 1 4 6 --out <outfile>
+plink2 --pfile GRCh37_1000G_ALL --score score_files/ea.txt 1 4 6 --out 1kgenomes
