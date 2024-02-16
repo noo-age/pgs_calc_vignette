e85d8c856ab6809b93a3b0060185883dc5c05494
Antelope10 <yejun.yun2006@gmail.com>
2024-02-16 11:35:36 -0600

    takes only bgzipped input now, also fixed

--- filter_split_vcf.sh
+++ filter_split_vcf.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
-# Check if an input file was provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <input_vcf.gz>"
+# Check if an input file and output directory were provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <input_vcf.gz> <outdir>"
     exit 1
 fi
 
 # The input VCF file from the first command line argument
-input_vcf="$1"
+input_vcf=$(basename $1)
+input_dir=$(dirname $1)
+
+# The output directory from the second command line argument
+outdir="$2"
+
+# Create the output directory if it doesn't exist
+mkdir -p "$outdir"
 
 # Loop through chromosomes 1-22
-for i in {1..22}
+for i in {1..22} X Y
 do
     # Define chromosome name
     chr="chr${i}"
-    
-    # Extract the base name of the input file without its extension
-    base_name=$(basename "$input_vcf" .vcf.gz)
-    
+
     # Define output file name
-    output_vcf="${chr}_${base_name}.vcf.gz"
+    output_vcf="${outdir}/${chr}_${input_vcf}"
     
     # Filter the input VCF for the current chromosome and compress the output
-    bcftools view "$input_vcf" --regions "$chr" | bgzip > "$output_vcf"
+    bcftools view ${input_dir}/${input_vcf} --regions "$chr" | bgzip > "$output_vcf"
     
     # Index the output VCF file
-    tabix -p vcf "$output_vcf"
+    bcftools index "$output_vcf"
 done
 
-echo "Chromosome VCF files have been generated."
+echo "Chromosome VCF files have been generated in $outdir."
