fb6ca326a672b70c0925053fd31b243f383fc7dd
Antelope10 <yejun.yun2006@gmail.com>
2024-02-16 11:35:20 -0600

    takes only bgzip input now, also fixed

--- duplicate_sample.sh
+++ duplicate_sample.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
 # Check if correct number of arguments provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <input_vcf> <output_vcf>"
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <input.vcf.gz>"
     exit 1
 fi
 
 # Input and output from command line arguments
-input_vcf="$1"
-output_vcf="$2"
+input_vcf=$(basename $1)
+echo $input_vcf
+input_dir=$(dirname $1)/
+echo $input_dir
+
+# Check to see if duplication is necessary
+num_samples=$(bcftools query -l "${input_dir}${input_vcf}" | wc -l)
+if [ "$num_samples" -gt 20 ]; then
+    echo "Input VCF has more than 20 samples. Duplication unnecessary to use the Michigan imputation server."
+    #exit 0
+fi
 
 # Get the sample name from the input VCF
-sample=$(bcftools query -l "$input_vcf")
+sample=$(bcftools query -l "${input_dir}${input_vcf}" | head -n 1)
 
 # An array to store the names of the temporary VCF files
 temp_files=()
 
 # Create 20 copies of the VCF with different sample names
-for i in $(seq 1 20); do
+for i in $(seq 1 30); do
     # Create a new sample name for this copy
     new_sample="${sample}_copy_${i}"
     
@@ -26,16 +35,18 @@ for i in $(seq 1 20); do
     temp_files+=("$temp_file")
     
     # Use bcftools reheader to change the sample name
-    bcftools reheader -s <(echo "$new_sample") -o "$temp_file" "$input_vcf"
+    bcftools reheader -s <(echo "$new_sample") -o "$temp_file" "${input_dir}${input_vcf}"
     bcftools index "$temp_file"
 done
 
 # Merge the temporary VCF files into the output VCF
-bcftools merge "${temp_files[@]}" -Oz -o "$output_vcf"
-bcftools index "$output_vcf"
+bcftools merge "${temp_files[@]}" -f -Oz -o "${input_dir}dup-${input_vcf}"
+bcftools index ${input_dir}dup-${input_vcf}
 
-# Clean up the temporary VCF files
-rm "${temp_files[@]}"
-rm "${temp_files[@]}.csi"
+# Clean up the temporary files
+for temp_file in "${temp_files[@]}"; do
+    rm "$temp_file"
+    rm "${temp_file}.csi" 
+done
 
-echo "Finished duplicating the sample in the VCF file."
+echo "Sample duplication complete"
